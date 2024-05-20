c7b0513b8b9d25d23d193518761abfbce39196a0
nestoralfaro <nalfarocs@gmail.com>
2024-05-20 03:13:09 +0000

    yet another attempt at my start-stop script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -2,21 +2,21 @@
 
 case "$1" in
   start)
-    echo "Starting aesdsocket"
-    start-stop-daemon -S -n aesdsocket -a aesdsocket -- -d
+    echo ">>>>>>>> Starting aesdsocket <<<<<<<<"
+    start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
     # start-stop-daemon --start --exec /usr/bin/aesdsocket -- -d
-    echo "Started aesdsocket"
+    echo ">>>>>>>> Started aesdsocket <<<<<<<<"
     ;;
   stop)
-    echo "Stopping aesdsocket"
+    echo ">>>>>>>> Stopping aesdsocket <<<<<<<<"
     start-stop-daemon -K -n aesdsocket
     # start-stop-daemon --stop --exec /usr/bin/aesdsocket
-    echo "Stopped aesdsocket"
+    echo ">>>>>>>> Stopped aesdsocket <<<<<<<<"
     ;;
   *)
     echo "Usage: $0 { start | stop }"
-    exit 1
-    ;;
+  exit 1
+    # ;;
 esac
 
 # exit 0
