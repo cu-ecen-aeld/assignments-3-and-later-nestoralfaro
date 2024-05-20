d88c73898a067d211dfa63c4bc8bcf1ea7fe1e4a
nestoralfaro <nalfarocs@gmail.com>
2024-05-20 01:55:22 +0000

    changed startup script

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -1,17 +1,22 @@
 #!/bin/sh
+
 case "$1" in
   start)
     echo "Starting aesdsocket"
-    # start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
     start-stop-daemon -S -n aesdsocket -a aesdsocket -- -d
+    # start-stop-daemon --start --exec /usr/bin/aesdsocket -- -d
+    echo "Started aesdsocket"
     ;;
   stop)
     echo "Stopping aesdsocket"
     start-stop-daemon -K -n aesdsocket
+    # start-stop-daemon --stop --exec /usr/bin/aesdsocket
+    echo "Stopped aesdsocket"
     ;;
   *)
-    echo "Usage: $0 {start|stop}"
-  exit 1
+    echo "Usage: $0 { start | stop }"
+    exit 1
+    ;;
 esac
 
-exit 0
+# exit 0
