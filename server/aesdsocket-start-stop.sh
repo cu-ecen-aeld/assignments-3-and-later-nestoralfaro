f73bf6655f3b146cd4ef7eabc341cf7f27e173e9
nestoralfaro <nalfarocs@gmail.com>
2024-05-27 19:05:09 +0000

    for a8

--- server/aesdsocket-start-stop.sh
+++ server/aesdsocket-start-stop.sh
@@ -2,20 +2,17 @@
 
 case "$1" in
   start)
-    echo ">>>>>>>> RECENT COMMIT Starting aesdsocket <<<<<<<<"
+    echo ">>>>>>>> Starting aesdsocket <<<<<<<<"
     start-stop-daemon -S -n aesdsocket -a /usr/bin/aesdsocket -- -d
-    # start-stop-daemon --start --exec /usr/bin/aesdsocket -- -d
-    echo ">>>>>>>> RECENT COMMIT Started aesdsocket <<<<<<<<"
+    echo ">>>>>>>> Started aesdsocket <<<<<<<<"
     ;;
   stop)
     echo ">>>>>>>> Stopping aesdsocket <<<<<<<<"
     start-stop-daemon -K -n aesdsocket
-    # start-stop-daemon --stop --exec /usr/bin/aesdsocket
     echo ">>>>>>>> Stopped aesdsocket <<<<<<<<"
     ;;
   *)
-    echo "Usage: $0 <start | stop>"
+    echo "Usage: $0 <start|stop>"
   exit 1
-    # ;;
 esac
 exit 0
