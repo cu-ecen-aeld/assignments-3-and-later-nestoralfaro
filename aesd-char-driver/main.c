ad278d8dec1af3f74621b3df7c28ff8a9a7d080c
nestoralfaro <nalfarocs@gmail.com>
2024-05-27 01:06:22 +0000

    fixed header includes

--- aesd-char-driver/main.c
+++ aesd-char-driver/main.c
@@ -18,10 +18,13 @@
 #include <linux/types.h>
 #include <linux/cdev.h>
 #include <linux/fs.h> // file_operations
-#include <stdint.h>
-#include <stdio.h>
+#include <linux/slab.h>
+#include <linux/uaccess.h>
+// #include <stdint.h>
+// #include <stdio.h>
 // #include "aesd-circular-buffer.h"
 #include "aesdchar.h"
+
 int aesd_major =   0; // use dynamic major
 int aesd_minor =   0;
 
