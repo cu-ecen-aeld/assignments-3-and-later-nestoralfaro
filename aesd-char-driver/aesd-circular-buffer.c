35ef850965decc1ec4ae0dc2f112e73aaa43f6c1
nestoralfaro <nalfarocs@gmail.com>
2024-05-27 01:21:14 +0000

    fixed  return type

--- aesd-char-driver/aesd-circular-buffer.c
+++ aesd-char-driver/aesd-circular-buffer.c
@@ -64,10 +64,13 @@ struct aesd_buffer_entry *aesd_circular_buffer_find_entry_offset_for_fpos(struct
 * Any necessary locking must be handled by the caller
 * Any memory referenced in @param add_entry must be allocated by and/or must have a lifetime managed by the caller.
 */
-void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
+const char *aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const struct aesd_buffer_entry *add_entry)
 {
+  const char *overwritten_buffptr = NULL;
+
   if (buffer->full) {
     // if buffer is full, overwrite the entry at out_offs
+    overwritten_buffptr = buffer->entry[buffer->out_offs].buffptr;
     buffer->out_offs = (buffer->out_offs + 1) % AESDCHAR_MAX_WRITE_OPERATIONS_SUPPORTED;
   }
 
@@ -79,6 +82,8 @@ void aesd_circular_buffer_add_entry(struct aesd_circular_buffer *buffer, const s
 
   // if in_offs equals out_offs, the buffer is full
   buffer->full = (buffer->in_offs == buffer->out_offs);
+
+  return overwritten_buffptr;
 }
 
 /**
